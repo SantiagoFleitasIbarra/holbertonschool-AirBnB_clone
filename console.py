8451710b59a9da57d55141c1cdcc2988d636f303
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-03-02 02:07:07 +0000

    add task

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""The console"""
+
+
+import cmd  # importing the cmd module
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """Quit command to exit the program\n"""
+        return True
+
+    def do_EOF(self, line):
+        """EOF command to exit the program\n"""
+        print()
+        return True
+
+    def emptyline(self):
+        """Empty line"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
