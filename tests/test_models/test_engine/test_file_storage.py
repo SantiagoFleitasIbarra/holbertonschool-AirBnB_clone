e752be2b7ef30818a7dfb2474d305604d324ce97
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-03-02 04:02:39 +0000

    add structure

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Test FileStorage"""
+
+
+import unittest
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
