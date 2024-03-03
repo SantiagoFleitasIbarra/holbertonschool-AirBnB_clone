77300ff5db8501646812ffdcfb0148e5d8669cf2
RomiPla <plaromina1997@gmail.com>
2024-03-02 21:58:15 -0300

    update

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """User class inherits from BaseModel """
+    """User class inherits from BaseModel"""
     password = ""
     email = ""
     first_name = ""
