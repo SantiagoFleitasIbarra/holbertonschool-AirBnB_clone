6fdf2cdb8929b7d99f77676731ce5495964e41c1
RomiPla <plaromina1997@gmail.com>
2024-03-02 21:52:38 -0300

    update

---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Create User """
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """User class inherits from BaseModel """
+    password = ""
+    email = ""
+    first_name = ""
+    last_name = ""
